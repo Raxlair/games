ca3aaf35ac3cf34ba93df178ccac98f04c1a0200
Raxlair <hotsexybeast@proton.me>
2025-12-29 15:17:03 +0000

    add clown movement, health display WIP

--- script.py
+++ script.py
@@ -8,6 +8,12 @@ class Character():
 
 class Player(Character):
     
+    def __init__(self):
+        self.x=0
+        self.y=0
+
+        self.health = 100
+    
     def Movement(self, playerSpeed):
         keys = pygame.key.get_pressed() 
         if keys[pygame.K_a]: 
@@ -24,8 +30,18 @@ class Player(Character):
         screen.blit(playerIcon,(self.x,self.y))
 
     def checkDeath(self):
-        if player.x == enemy1.x and player.y == enemy1.y:
+        if self.x == enemy1.x and self.y == enemy1.y or self.health <= 0:
             print("you are dead")
+
+    def showHealth(self):
+        xHealth = 400
+        yHealth = 400
+        
+        #textRender = Font.render("HEALTH:"+str(self.health),True, white)
+        #screen.blit(textRender,(xHealth,yHealth))
+
+        textSurface = Font.render(f"Score: {self.health}", True, (255, 255, 255))
+        screen.blit(textSurface, (100, 100))
 
 class Enemy(Character):
     def __init__(self):
@@ -37,27 +53,48 @@ class Enemy(Character):
     def draw(self):
         screen.blit(self.enemyIcon,(self.x,self.y))
 
+    def movement(self, enemySpeed):
+        self.xDestination = player.x
+        self.yDestination = player.y
+
+        if self.x < self.xDestination:
+            self.x = self.x + enemySpeed
+        if self.x > self.xDestination:
+            self.x = self.x - enemySpeed
+        if self.y < self.yDestination:
+            self.y = self.y + enemySpeed
+        if self.y > self.yDestination:
+            self.y = self.y - enemySpeed
+
 #initialise pygame
 pygame.init()
 #start game window
 screen = pygame.display.set_mode((400, 400), pygame.RESIZABLE)
-pygame.display.set_caption("Pygame")
+pygame.display.set_caption("stock :?")
 pygame_icon = pygame.image.load("images/icon.png")
 pygame.display.set_icon(pygame_icon)
 
 screen.fill((0, 0, 0))
 pygame.display.flip()
 
+#handle background
 bg = pygame.image.load("images/cat.png").convert()
 bg = pygame.transform.scale(bg ,(1920,1080))
 
-screen.blit(bg,(0,0)) 
-pygame.display.update()
+#handles the font settings
+Font=pygame.font.SysFont('timesnewroman',  30)
+white = (255, 255, 255)
+green = (0, 255, 0)
+transparent = (0,0,0)
 
 player = Player()
 enemy1 = Enemy()
-enemy2 = Enemy()
-enemy3 = Enemy()
+
+player.showHealth()
+pygame.display.flip
+
+screen.blit(bg,(0,0)) 
+pygame.display.update()
 
 running = True
 while running:
@@ -66,13 +103,11 @@ while running:
             running = False
 
     screen.blit(bg,(0,0))    
-    player.Movement(30)
+    player.Movement(20)
     enemy1.draw()
-    enemy2.draw()
-    enemy3.draw()
     pygame.display.update()
 
     player.checkDeath()
-    print("a")
+    enemy1.movement(5)
 
 pygame.quit()
