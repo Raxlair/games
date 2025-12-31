979c284cc0f532d8490a69af3dc8441ff4b8b4f2
Raxlair <hotsexybeast@proton.me>
2025-12-31 16:09:25 +0000

    added difficulty, better hitboxes and diagonal movement

--- script.py
+++ script.py
@@ -18,16 +18,18 @@ class Player(Character):
         self.score = 0
 
         self.death = False
+        self.difficulty = ""
+        self.difficultySpeed = 0
     
     def Movement(self, playerSpeed):
         keys = pygame.key.get_pressed() 
         if keys[pygame.K_a]: 
             self.x = self.x - playerSpeed
-        elif keys[pygame.K_d]:
+        if keys[pygame.K_d]:
             self.x = self.x + playerSpeed
-        elif keys[pygame.K_w]:
+        if keys[pygame.K_w]:
             self.y = self.y - playerSpeed
-        elif keys[pygame.K_s]:
+        if keys[pygame.K_s]:
             self.y = self.y + playerSpeed
         
         playerIcon = pygame.image.load("images/player.png").convert()
@@ -92,7 +94,29 @@ class Player(Character):
         textSurface = Font.render(f"Current Highscore: {self.highscore} points", True, (255, 255, 255))
         screen.blit(textSurface, (xScore, yHighscore))   
 
+    def selectDifficulty(self):
+        screen.fill((0, 0, 0))
+        textSurface = Font.render("Press button to select difficulty", True, (255, 255, 255))
+        screen.blit(textSurface, (800, 300))
+        textSurface = Font.render(f"1 - Easy", True, (255, 255, 255))
+        screen.blit(textSurface, (800, 400))
+        textSurface = Font.render(f"2 - Medium", True, (255, 255, 255))
+        screen.blit(textSurface, (800, 500))       
+        textSurface = Font.render(f"3 - Hard", True, (255, 255, 255))
+        screen.blit(textSurface, (800, 600)) 
+        pygame.display.update()
 
+        keys = pygame.key.get_pressed() 
+        if keys[pygame.K_1]: 
+            self.difficulty = "easy"
+            self.difficultySpeed = 50
+        if keys[pygame.K_2]:
+            self.difficulty = "medium"
+            self.difficultySpeed = 25
+        if keys[pygame.K_3]:
+            self.difficulty = "hard"
+            self.difficultySpeed = 10  
+        
 class Enemy(Character):
     def __init__(self):
         self.x=random.randint(0,1800)
@@ -109,7 +133,7 @@ class Enemy(Character):
         self.xDestination = player.x
         self.yDestination = player.y
 
-        enemySpeed = player.score/25
+        enemySpeed = player.score/player.difficultySpeed
 
         if self.x < self.xDestination:
             self.x = self.x + enemySpeed
@@ -121,7 +145,7 @@ class Enemy(Character):
             self.y = self.y - enemySpeed
     
     def attack(self, damage, range):
-        if (player.x - range <= self.x <= player.x + range and player.y - range <= self.y <= player.y + range):
+        if (player.x+175 - range <= self.x <= player.x+175 + range and player.y+118.75 - range <= self.y <= player.y+118.75 + range):
             player.health = player.health - damage
             mixer.music.load("audio/damage.mp3")
             mixer.music.set_volume(1)
@@ -156,12 +180,7 @@ Font=pygame.font.SysFont('timesnewroman',  30)
 player = Player()
 enemy1 = Enemy()
 enemy2 = Enemy()
-
-player.showHealth()
-pygame.display.flip
-
-screen.blit(bg,(0,0)) 
-pygame.display.update()
+enemy3 = Enemy()
 
 running = True
 
@@ -170,23 +189,87 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.blit(bg,(0,0))    
-    player.Movement(40)
+    player.selectDifficulty()
+
+    if player.difficulty == "easy":
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            
+            screen.blit(bg,(0,0))    
+            player.Movement(40)
+
+            enemy1.draw()
+            enemy1.movement()
+            enemy1.attack(5,100)
+
+            player.showHealth()
+            player.showScore()
+
+            pygame.display.update()
+            enemy1.reset()
+            player.checkDeath()
+        
+        pygame.quit()
     
-    enemy1.draw()
-    enemy1.movement()
-    enemy1.attack(5,50)
+    elif player.difficulty == "medium":
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            
+            screen.blit(bg,(0,0))    
+            player.Movement(40)
 
-    enemy2.draw()
-    enemy2.movement()
-    enemy2.attack(5,50)
+            enemy1.draw()
+            enemy1.movement()
+            enemy1.attack(5,100)
 
-    player.showHealth()
-    player.showScore()
+            enemy2.draw()
+            enemy2.movement()
+            enemy2.attack(5,100)
 
-    pygame.display.update()
-    enemy1.reset()
-    enemy2.reset()
-    player.checkDeath()
+            player.showHealth()
+            player.showScore()
+
+            pygame.display.update()
+            enemy1.reset()
+            enemy2.reset()
+            player.checkDeath()
+        
+        pygame.quit()
+
+    elif player.difficulty == "hard":
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            
+            screen.blit(bg,(0,0))    
+            player.Movement(40)
+
+            enemy1.draw()
+            enemy1.movement()
+            enemy1.attack(5,100)
+
+            enemy2.draw()
+            enemy2.movement()
+            enemy2.attack(5,100)
+
+            enemy3.draw()
+            enemy3.movement()
+            enemy3.attack(5,100)
+
+            player.showHealth()
+            player.showScore()
+
+            pygame.display.update()
+            enemy1.reset()
+            enemy2.reset()
+            enemy3.reset()
+            player.checkDeath()
+        
+        pygame.quit()
 
 pygame.quit()
